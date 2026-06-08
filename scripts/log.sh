58c84c225ee1c971d487d1bb37661eaf67126b19
Brennan Wilkes <brennan@codexwilkes.com>
2026-06-08 11:50:07 -0700

    chore: Bugfix script

--- scripts/log.sh
+++ scripts/log.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd "$(git -C "$(dirname "$0")" rev-parse --show-toplevel)/worker" || exit 1
-npx wrangler tail --format pretty 2>&1 | grep "(log)"
+npx wrangler tail --format pretty 2>&1 | grep '\(log\)'
